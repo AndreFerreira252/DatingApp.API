9a48cf4d41c87d310b632a23ba605722ae614a6a
Andre Ferreira <amrpgdx@gmail.com>
2018-03-17 17:32:31 +0000

    Added Messages

--- Data/DataContext.cs
+++ Data/DataContext.cs
@@ -18,6 +18,8 @@ namespace DatingApp.API.Data
 
         public DbSet<Like> Likes { get; set; }
 
+        public DbSet<Message> Messages { get; set; }
+
         protected override void OnModelCreating(ModelBuilder builer)
         {
             builer.Entity<Like>()
@@ -33,6 +35,16 @@ namespace DatingApp.API.Data
             .HasOne(u => u.Liker)
             .WithMany(u => u.Likee)
             .HasForeignKey(u => u.LikeeId)
+            .OnDelete(DeleteBehavior.Restrict);
+
+            builer.Entity<Message>()
+            .HasOne(u => u.Sender)
+            .WithMany(u => u.MessagesSent)
+            .OnDelete(DeleteBehavior.Restrict);
+
+            builer.Entity<Message>()
+            .HasOne(u => u.Recipient)
+            .WithMany(u => u.MessagesReceived)
             .OnDelete(DeleteBehavior.Restrict);
         }
     }
--- Helpers/AutoMapperProfiles.cs
+++ Helpers/AutoMapperProfiles.cs
@@ -24,12 +24,18 @@ namespace DatingApp.API.Helpers
                 .ForMember(
                     dest => dest.Age,
                     opt => opt.ResolveUsing(d => d.DateOfBirth.CalculateAge()));
-                    
+
             CreateMap<Photo, PhotosForDetailDto>();
             CreateMap<UserForUpdateDto, User>();
             CreateMap<PhotoForCreationDto, Photo>();
             CreateMap<Photo, PhotoForReturnDto>();
             CreateMap<UserForRegisterDto, User>();
+            CreateMap<MessageForCreationDto, Message>().ReverseMap();
+            CreateMap<Message, MessageToReturnDto>()
+                .ForMember(m => m.SenderPhotoUrl, opt => opt
+                    .MapFrom(u => u.Sender.Photos.FirstOrDefault(p => p.IsMain).Url))
+                .ForMember(m => m.RecipientPhotoUrl, opt => opt
+                    .MapFrom(u => u.Recipient.Photos.FirstOrDefault(p => p.IsMain).Url));
         }
     }
 }
